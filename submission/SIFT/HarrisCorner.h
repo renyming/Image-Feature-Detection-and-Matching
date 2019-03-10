df5709724f21a1829ae9400a8090c2ac9ecdd17a
Yiming Ren <renyming@gmail.com>
2019-03-10 18:05:36 -0400

    Port code to cmake project

--- submission/SIFT/HarrisCorner.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include<opencv2/opencv.hpp>
-#include<vector>
-#include "ANMS.h"
-
-void harrisCorner(const cv::Mat&, std::vector<cv::KeyPoint>&, int, bool);
-void localMaxima(const cv::Mat& cNorm, std::vector<cv::KeyPoint>& corners, int threshold);
